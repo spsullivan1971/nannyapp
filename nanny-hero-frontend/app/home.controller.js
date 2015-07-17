1d2953f4ab10078d09ac91e0736bf7c84972942a
Sam Grayson <samuel.c.grayson17@gmail.com>
2015-07-17 19:35:37 -0400

    styling

--- nanny-hero-frontend/app/home.controller.js
+++ nanny-hero-frontend/app/home.controller.js
@@ -2,7 +2,7 @@
   'use strict';
   angular
     .module('nannyHero')
-    .controller('HomeController', function($scope, $routeParams, $rootScope, FamilyService){
+    .controller('HomeController', function($scope, $routeParams, $rootScope, FamilyService, $modal, $log, $location){
 
         $scope.navUser = function() {
 
@@ -19,10 +19,39 @@
           })
         };
         $scope.fetchFamilyInfo = function(familyName) {
-          FamilyService.getFamily(familyName).then(function(data) {
-            $rootScope.familyInfo = info.data[0];
-            console.log(info.data[0])
+          FamilyService.getFamily(familyName).success(function(info) {
+            $rootScope.familyInfo = info;
+            console.log(info)
+            $location.path('/myFamily');
+          }).error(function(data){
+            $location.path('/addFamily');
           })
         }
+
+        $scope.animationsEnabled = true;
+
+        $scope.open = function (size, data) {
+
+        var modalInstance = $modal.open({
+          animation: $scope.animationsEnabled,
+          templateUrl: 'app/family/views/modal.html',
+          controller: 'ModalInstanceCtrl'
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+          $scope.selected = selectedItem;
+        }, function () {
+          $log.info('Modal dismissed at: ' + new Date());
+        });
+        };
+
+        })
+
+        .controller('ModalInstanceCtrl', function ($scope, $modalInstance) {
+
+        $scope.close = function () {
+          $modalInstance.close($scope.selected);
+        };
+
     });
 })();
